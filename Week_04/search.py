4b2a298b36aff5665e80f8600f6a0c16a5c63271
chengsc <thatwhyme@gmail.com>
2020-08-01 21:36:17 +0800

    homework4

---
+++ Week_04/search.py
@@ -0,0 +1,24 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if not nums:
+            return -1
+        i, j =0, len(nums)-1
+        while i<=j:
+            mid = (i + j)//2
+            if nums[mid] == target:
+                return mid
+
+            if nums[0] <= nums[mid] :
+                if nums[0] <= target <= nums[mid]:
+                    j = mid - 1
+                else:
+                    i = mid + 1
+
+            else:
+                if nums[mid]<=target<=nums[len(nums)-1]:
+                    i = mid + 1
+                else:
+                    j = mid - 1
+        return -1
+                
+
